c9dc097443cc64f5710daa1ed21dee0344507dc8
Mario Junior <65376066+mariojljunior@users.noreply.github.com>
2020-09-26 19:02:51 -0400

    inicio

---
+++ ola.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Primeiro exemplo de PHP</title>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php
+        echo "<h2>Olá, <br>mundo!</h2>";
+    ?>
+</body>
+</html>
