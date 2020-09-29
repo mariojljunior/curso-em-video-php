f04f63873c338e4c3a80b5821e91285664982ec2
Mario Junior <65376066+mariojljunior@users.noreply.github.com>
2020-09-28 21:07:16 -0400

    aula06-02

---
+++ variaveisvariaveis.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estiloCEV.css">
+    <title>Variáveis de variáveis</title>
+</head>
+<body>
+    <div>
+        <?php
+            $x = "abc";
+            $$x = "def";
+            echo "O conteúdo da variável X é $x";
+            echo "<br>A variável ABC criada recebeu o valor $abc";
+        ?>
+    </div>
+</body>
+</html>
