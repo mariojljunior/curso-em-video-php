2d437b7f1dc1054e9bec208b86d8f5ab1a63fd4b
Mario Junior <65376066+mariojljunior@users.noreply.github.com>
2020-12-23 17:40:41 -0400

    aula10

---
+++ aula10/exercicio03.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/estiloCEV.css">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <?php
+            
+        ?>
+        <br><br>
+        <a href="exercicio.html">Voltar</a>
+    </div>
+</body>
+</html>
